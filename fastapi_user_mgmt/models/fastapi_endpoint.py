f6ceaf6869ab452c683350961b77ac71c5ff9604
Yoshi Tashiro <tashiro@quartile.co>
2025-06-10 13:03:03 +0000

    remove _get_app_dependencies_overrides()

--- fastapi_user_mgmt/models/fastapi_endpoint.py
+++ fastapi_user_mgmt/models/fastapi_endpoint.py
@@ -3,14 +3,6 @@
 
 from odoo import fields, models
 
-from odoo.addons.fastapi.dependencies import (
-    authenticated_partner_from_basic_auth_user,
-    authenticated_partner_impl,
-)
-from odoo.addons.fastapi_auth_user_apikey.models.fastapi_endpoint import (
-    api_key_based_authenticated_partner_impl_custom,
-)
-
 from ..routers.user_router import router as userapi_router
 
 
@@ -26,16 +18,3 @@ class FastapiEndpoint(models.Model):
         if self.app == "userapi":
             return [userapi_router]
         return super()._get_fastapi_routers()
-
-    def _get_app_dependencies_overrides(self):
-        res = super()._get_app_dependencies_overrides()
-        if self.app == "userapi":
-            if self.auth_method == "http_basic":
-                res[authenticated_partner_impl] = (
-                    authenticated_partner_from_basic_auth_user
-                )
-            else:  # "api_key"
-                res[authenticated_partner_impl] = (
-                    api_key_based_authenticated_partner_impl_custom
-                )
-        return res
