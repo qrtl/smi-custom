0aeec3b8506b5b6966de6ec6367be2ed3b151a39
Yoshi Tashiro <tashiro@quartile.co>
2025-06-18 10:48:09 +0000

    [5453][ADD] partner_line_attribute

---
+++ partner_line_attribute/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2025 Quartile (https://www.quartile.co)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner LINE Attributes",
+    "category": "Extra Tools",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Quartile",
+    "website": "https://www.quartile.co",
+    "depends": ["base"],
+    "data": ["views/res_partner_views.xml"],
+    "installable": True,
+}
---
+++ partner_line_attribute/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+This module adds LINE related fields to the partner form.
